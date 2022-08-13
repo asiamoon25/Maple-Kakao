fcabde937f07b62085e697eb5bb1a1426ce520bc
첫 커밋 <>
2022-08-13 19:48:24 +0900

    commit

--- src/main/java/me/maplechat/maplekakao/controller/UserController.java
+++ src/main/java/me/maplechat/maplekakao/controller/UserController.java
@@ -1,5 +1,6 @@
 package me.maplechat.maplekakao.controller;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,6 +9,8 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,35 +47,52 @@ public class UserController {
             String workRankWorld ;
             String workRankAll;
             for (Element e : elem) {
-                System.out.println(e.select("img.character-image").attr("src")); //Avatar Image
-                System.out.println(e.select("b.align-middle").get(0).text()); // username
 
-                System.out.println(e.select("ul.user-summary-list").select("li.user-summary-item").get(0).text()); // Lv
-                System.out.println(e.select("ul.user-summary-list").select("li.user-summary-item").get(1).text()); // 직업
-                System.out.println(e.select("ul.user-summary-list").select("li.user-summary-item").get(2).text()); // 인기도
-
-                System.out.println(e.select("div.col-lg-8").select("a.text-yellow").text()); //길드 명
-
-                System.out.println(e.select("div.col-lg-8").select("span").get(2).text());// 종합랭킹
-                System.out.println(e.select("div.col-lg-8").select("span").get(3).text());// 월드 랭킹
-                System.out.println(e.select("div.col-lg-8").select("span").get(4).text());// 직업 랭킹(월드)
-                System.out.println(e.select("div.col-lg-8").select("span").get(5).text());// 직업 랭킹(전체)
-                imgUrl =e.select("img.character-image").attr("src");
-                 username1 = e.select("b.align-middle").get(0).text();
-                 level = e.select("ul.user-summary-list").select("li.user-summary-item").get(0).text();
-                 work= e.select("ul.user-summary-list").select("li.user-summary-item").get(1).text();
-                 popular = e.select("ul.user-summary-list").select("li.user-summary-item").get(2).text();
-                 guild = e.select("div.col-lg-8").select("a.text-yellow").text();
-                 totalRank = e.select("div.col-lg-8").select("span").get(2).text();
-                 worldRank = e.select("div.col-lg-8").select("span").get(3).text();
-                 workRankWorld = e.select("div.col-lg-8").select("span").get(4).text();
-                 workRankAll = e.select("div.col-lg-8").select("span").get(5).text();
+                 imgUrl =e.select("img.character-image").attr("src"); // 아바타 이미지
+                 username1 = e.select("b.align-middle").get(0).text();// 유저 네임
+                 level = e.select("ul.user-summary-list").select("li.user-summary-item").get(0).text();// 레벨
+                 work= e.select("ul.user-summary-list").select("li.user-summary-item").get(1).text(); //직업
+                 popular = e.select("ul.user-summary-list").select("li.user-summary-item").get(2).text(); // 인기도
+                 guild = e.select("div.col-lg-8").select("a.text-yellow").text(); // 길드
+                 totalRank = e.select("div.col-lg-8").select("span").get(2).text(); // 전체 랭킹
+                 worldRank = e.select("div.col-lg-8").select("span").get(3).text(); // 월드 랭킹
+                 workRankWorld = e.select("div.col-lg-8").select("span").get(4).text(); // 월드 직업 랭킹
+                 workRankAll = e.select("div.col-lg-8").select("span").get(5).text(); // 전체 직업 랭킹
 
 
             }
-            json.put("imgUrl",imgUrl);
-            json.put("username1",username1);
-            json.put("level",level);
+          //json 에 json array(JSON) 그안에 json
+            JSONObject template = new JSONObject();
+            JSONObject basicCard = new JSONObject();
+            JSONObject thumbNailImgUrl = new JSONObject();
+            JSONObject buttons1 = new JSONObject();
+            ArrayList<JSONObject> buttons = new ArrayList<>();
+            ArrayList<JSONObject> outputs = new ArrayList<JSONObject>();
+
+            buttons1.put("action","webLink");
+            buttons1.put("label","웹에서 보기");
+            buttons1.put("webLinkUrl",baseUrl);
+
+            buttons.add(buttons1);
+
+            thumbNailImgUrl.put("imageUrl",imgUrl);
+
+
+
+
+            basicCard.put("title","캐릭터 정보");
+            basicCard.put("description",username1);
+            basicCard.put("thumbnail",thumbNailImgUrl);
+
+            outputs.add(basicCard);
+
+            template.put("outputs",outputs);
+
+            json.put("version","2.0");
+            json.put("template",template);
+
+            return json;
+
         }catch (Exception e){
             e.printStackTrace();
         }
